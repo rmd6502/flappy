e3b77e2b00ac98e1794a75d0f1c97653a44eded2
Robert Diamond <rmd6502@gmail.com>
2014-08-21 08:27:18 -0400

    add header

---
+++ firmware/flappy.h
@@ -0,0 +1,11 @@
+const int start_button = 0;
+const int hall_input = 1;
+const int led = 2;
+const int adc_reference = 3;
+const int sck = 4;
+const int miso = 5;
+const int mosi = 6;
+const int speaker = 7;
+const int lid_servo = 8;
+const int character_servo = 9;
+const int roll_servo = 10;
